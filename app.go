544c5d335566f0f4616eccc2e3602d27ad0f7b89
Jared Chavez <jared@undeadlabs.net>
2019-02-07 18:47:03 -0800

    add go.mod. pid must be active and be the same binary name in order to force an early app exit

--- app.go
+++ app.go
@@ -2,7 +2,7 @@
 //
 //  app.go
 //
-//  Copyright (c) 2014, Jared Chavez. 
+//  Copyright (c) 2014, Jared Chavez.
 //  All rights reserved.
 //
 //  Use of this source code is governed by a BSD-style
@@ -15,49 +15,52 @@
 package app
 
 import (
-    "path/filepath"
-    "os"
-    "os/exec"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	ps "github.com/mitchellh/go-ps"
 )
 
 // GetExeDir returns the base directory of the executing binary.
 func GetExeDir() string {
-    return filepath.Dir(GetExePath())
+	return filepath.Dir(GetExePath())
 }
 
 // GetExeFile returns the file name of the executing binary.
 func GetExeFile() string {
-    return filepath.Base(GetExePath())
+	return filepath.Base(GetExePath())
 }
 
 // GetExePath attempts to find and return the executing binary's full path.
 func GetExePath() string {
-    rootPath := filepath.Dir(os.Args[0])
+	rootPath := filepath.Dir(os.Args[0])
 
-    if rootPath == "." {
-        rootPath, err := exec.LookPath(os.Args[0])
-        if err != nil {
-            return os.Args[0]
-        }
+	if rootPath == "." {
+		rootPath, err := exec.LookPath(os.Args[0])
+		if err != nil {
+			return os.Args[0]
+		}
 
-        rootPath, err = filepath.Abs(rootPath)
-        if err != nil {
-            return os.Args[0]
-        }
+		rootPath, err = filepath.Abs(rootPath)
+		if err != nil {
+			return os.Args[0]
+		}
 
-        return rootPath
-    }
-    
-    return os.Args[0]
+		return rootPath
+	}
+
+	return os.Args[0]
 }
 
 // GetName returns the name of the executing application, as parsed from
 // the executing binary's file name.
 func GetName() string {
-    fn  := GetExeFile()
-    ext := filepath.Ext(fn)
+	fn := GetExeFile()
+	ext := filepath.Ext(fn)
 
-    return fn[:len(fn) - len(ext)]
+	return fn[:len(fn)-len(ext)]
 }
 
 // GetRunStatus attempts to read the application pid file and check the status
@@ -66,23 +69,34 @@ func GetName() string {
 // is found, but the pid is no longer active, GetRunStatus will automatically
 // attempt to clean up the lingering pid file.
 func GetRunStatus() *os.Process {
-    pid, err := ReadPidFile()
-    if err != nil {
-        // can't find pid file
-        return nil
-    }
+	pid, err := ReadPidFile()
+	if err != nil {
+		// can't find pid file
+		return nil
+	}
 
-    p, err := os.FindProcess(pid)
-    if err != nil {
-        // pid file exists, but pid no longer does
-        DeletePidFile()
-        return nil
-    }
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		// pid file exists, but pid no longer does
+		DeletePidFile()
+		return nil
+	}
 
-    return p
+	proc, err := ps.FindProcess(pid)
+	if err != nil {
+		return p
+	}
+
+	if strings.ToLower(GetExeFile()) != strings.ToLower(proc.Executable()) {
+		// pid exists, but is tied to a different application now
+		DeletePidFile()
+		return nil
+	}
+
+	return p
 }
 
 // init initializes the package.
 func init() {
-    SetPidBaseDir(GetExeDir())
+	SetPidBaseDir(GetExeDir())
 }
